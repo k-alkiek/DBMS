676c7524c326ce93907b53034717bbd30a5dcabc
Ayman <ayman.a.samy.m@gmail.com>
2017-11-25 21:00:41 +0200

    Edited the design and added some interfaces and converted operations interfaces to calsses

--- src/data/IDatabase.java
+++ src/data/IDatabase.java
@@ -4,5 +4,6 @@ package data;
  * Created by khaled on 11/22/17.
  */
 public interface IDatabase {
-
+    void createTable(String tableName,IField[] fields);
+    void deleteTable(String tableName);
 }
--- src/operations/IDropDatabase.java
+++
@@ -1,8 +0,0 @@
-package operations;
-
-/**
- * Created by khaled on 11/20/17.
- */
-public interface IDropDatabase extends IBooleanOperation{
-    void setName(String name);
-}
