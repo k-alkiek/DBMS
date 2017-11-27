51fc7e3357396a4be299092d2c2a6f80750e2f11
Khaled Alkiek <k.alkiek@gmail.com>
2017-11-27 20:13:47 +0200

    modify factory, implement table delete

--- src/data/FieldsFactory.java
+++ src/data/FieldsFactory.java
@@ -7,8 +7,12 @@ import java.util.List;
  */
 public class FieldsFactory {
     public static IField create(String className, String fieldName) throws IllegalAccessException, InstantiationException {
-        if (className.equals("VarcharField")) return new VarcharField(fieldName);
-        else if (className.equals("IntField")) return new IntField(fieldName);
+        if (className.equals("VarcharField") || className.equals("varchar")) {
+            return new VarcharField(fieldName);
+        }
+        else if (className.equals("IntField") || className.equals("int")) {
+            return new IntField(fieldName);
+        }
         else {
             return null;
         }
