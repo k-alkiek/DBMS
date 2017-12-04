ef6dfc99daef4b63b85728f6c5941889f5661c74
Ayman <ayman.a.samy.m@gmail.com>
2017-12-04 07:30:14 +0200

    Finish CLI for JDBC

--- src/main/CommandLineInterface.java
+++ src/main/CommandLineInterface.java
@@ -6,6 +6,8 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -50,7 +52,35 @@ public class CommandLineInterface {
                 } else if (query.toLowerCase().startsWith("select")) {
                     try {
                         ResultSet resultSet = statement.executeQuery(query);
-                        // Todo
+                        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+                        System.out.println(resultSetMetaData.getTableName(0) + " :");
+                        int columnNumber = resultSetMetaData.getColumnCount();
+                        int rowNumber = 0;
+                        List<Object[]> table = new ArrayList<>();
+                        Object[] firstColumn = new Object[columnNumber];
+                        for (int i = 0; i < columnNumber; i++) {
+                            firstColumn[i] = resultSetMetaData.getColumnName(i+1);
+                        }
+                        table.add(firstColumn);
+                        rowNumber++;
+                        resultSet.first();
+                        while (!resultSet.isAfterLast())
+                        {
+                            Object[] column = new Object[columnNumber];
+                            for (int i = 0; i < columnNumber; i++) {
+                                column[i] = resultSet.getObject(i+1);
+                            }
+                            table.add(column);
+                            rowNumber++;
+                            resultSet.next();
+                        }
+                        Object[][] newTable = new Object[columnNumber][rowNumber];
+                        for(int i = 0;i < rowNumber;i++) {
+                            for(int j = 0;j < columnNumber;j++) {
+                                newTable[i][j] = table.get(i)[j];
+                            }
+                        }
+                        drawTable(newTable);
                     } catch (Exception e) {
                         log.error("Error");
                         System.out.println("ERROR");
@@ -70,6 +100,43 @@ public class CommandLineInterface {
             System.out.println("Error Connecting to the Driver");
         }
 
+
+    }
+
+    public static void drawTable(Object[][] values) {
+        List<Integer> lengths = new ArrayList<>();
+        for (int i = 0; i < values[0].length; i ++) {
+            int maxLen = -1;
+            for (int j = 0; j < values.length; j ++) {
+                maxLen = Math.max(maxLen, values[j][i].toString().length());
+            }
+            lengths.add(maxLen + 4);
+        }
+        for (int i = 0; i < values.length; i ++) {
+            System.out.print("+");
+            for (int j = 0; j < values[i].length; j++) {
+                for(int k = 0; k < lengths.get(j) - 2; k ++)
+                    System.out.print("-");
+                System.out.print("+");
+            }
+            System.out.println();
+            System.out.print("|");
+            for (int j = 0; j < values[i].length; j++) {
+                System.out.print(values[i][j].toString());
+                int length = values[i][j].toString().length();
+                for(int k = 0; k < lengths.get(j) - length - 2; k ++)
+                    System.out.print(" ");
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+        System.out.print("+");
+        for (int j = 0; j < values[0].length; j++) {
+            for(int k = 0; k < lengths.get(j) - 2; k ++)
+                System.out.print("-");
+            System.out.print("+");
+        }
+        System.out.println();
     }
 
 
